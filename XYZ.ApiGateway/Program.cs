42219c2c637e37d5ff52c20565ce9cb07288f906
A1EXF6A <aguachi4414@uta.edu.ec>
2025-10-05 22:58:28 -0500

    change in cors

--- XYZ.ApiGateway/Program.cs
+++ XYZ.ApiGateway/Program.cs
@@ -10,7 +10,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-// Configure CORS
+// CORS Configuration 
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
