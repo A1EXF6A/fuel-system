235197dc50a1d77fcae37a9a984f980bfc84f17e
RanMd <randommid1873@gmail.com>
2025-12-07 19:31:17 -0500

    fix: cors

--- XYZ.ApiGateway/Program.cs
+++ XYZ.ApiGateway/Program.cs
@@ -16,7 +16,7 @@ builder.Services.AddCors(options =>
     options.AddDefaultPolicy(policy =>
     {
         policy
-            .WithOrigins("http://localhost:3000")
+            .WithOrigins("http://localhost:3000", "https://blue-sea-0af160610.3.azurestaticapps.net")
             .AllowAnyMethod()
             .AllowAnyHeader()
             .AllowCredentials();
